c3e1e7c5ecc3cd587e5618ba574c8890c8226c67
Riyan <123riyanaje@gmail.com>
2024-11-17 22:31:08 +0700

    Update vercel.json

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,49 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    // User Routes
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::put('/user', [AuthController::class, 'update']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Post Routes
+    Route::get('/posts', [PostController::class, 'index']); // Get all posts
+    Route::post('/posts', [PostController::class, 'store']); // Create post
+    Route::get('/posts/{id}', [PostController::class, 'show']); // Get single post
+    Route::put('/posts/{id}', [PostController::class, 'update']); // Update post
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']); // Delete post
+
+    // Comment Routes
+    Route::get('/posts/{id}/comments', [CommentController::class, 'index']); // Get comments for a post
+    Route::post('/posts/{id}/comments', [CommentController::class, 'store']); // Add comment to a post
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']); // Delete comment
+
+    // Like Routes
+    Route::post('/posts/{id}/likes', [LikeController::class, 'likeOrUnLike']);
+     
+
+
 });
